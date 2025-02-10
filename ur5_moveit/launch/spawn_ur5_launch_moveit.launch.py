737db7853ef8d648c862ffe48aa70797df94b209
Arpit <123756415+ar-py-grim@users.noreply.github.com>
2025-02-10 16:21:45 +0530

    Add files via upload

--- ur5_moveit/launch/spawn_ur5_launch_moveit.launch.py
+++ ur5_moveit/launch/spawn_ur5_launch_moveit.launch.py
@@ -1,14 +1,9 @@
 import os
 import yaml
-import launch_ros
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.conditions import IfCondition
 from ament_index_python import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import TimerAction
-
+from launch_param_builder import ParameterBuilder
 
 def get_package_file(package, file_path):
     """Get the location of a file installed in an ament package"""
@@ -53,22 +48,26 @@ def generate_launch_description():
     ompl_config_file = get_package_file(moveit_config_folder_name, 'config/ompl_planning.yaml')
     moveit_controllers_file = get_package_file(moveit_config_folder_name, 'config/moveit_controllers.yaml')
     moveit_servo_file = get_package_file(moveit_config_folder_name, "config/ur_servo.yaml")
-    ros_controllers_file = get_package_file('ur5_moveit', 'config/ros_controllers.yaml')
+    moveit_rviz_file = get_package_file(moveit_config_folder_name,'config/moveit.rviz')
+    joint_limits_file = get_package_file('ur5_moveit', 'config/initial_positions.yaml')
 
     robot_description_semantic = load_file(srdf_file)
     kinematics_config = load_yaml(kinematics_file)
     ompl_config = load_yaml(ompl_config_file)
+    joint_limits_config = load_yaml(joint_limits_file)
 
     moveit_controllers = {
         'moveit_simple_controller_manager' : load_yaml(moveit_controllers_file),
         'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'
     }
+
     trajectory_execution = {
         'moveit_manage_controllers': True,
         'trajectory_execution.allowed_execution_duration_scaling': 1.2,
         'trajectory_execution.allowed_goal_duration_margin': 0.5,
         'trajectory_execution.allowed_start_tolerance': 0.01
     }
+
     planning_scene_monitor_config = {
         'publish_planning_scene': True,
         'publish_geometry_updates': True,
@@ -82,29 +81,35 @@ def generate_launch_description():
         executable='move_group',
         output='screen',
         parameters=[
-            {
-                'robot_description': robot_description_arm,
-                'robot_description_semantic': robot_description_semantic,
-                'robot_description_kinematics': kinematics_config,
-                'ompl': ompl_config,
-                'planning_pipelines': ['ompl'],
-            },
+            {"use_sim_time": True},
+            {'robot_description': robot_description_arm},
+            {'robot_description_semantic': robot_description_semantic},
+            {'robot_description_kinematics': kinematics_config},
+            {'ompl': ompl_config},
+            {'joint_limits': joint_limits_config},
+            {'planning_pipelines': ['ompl']},
             moveit_controllers,
             trajectory_execution,
             planning_scene_monitor_config,
-            {"use_sim_time": True},
         ],
     )
 
     # Servo node for realtime control
-    servo_yaml = load_yaml(moveit_servo_file)
-    servo_params = {"moveit_servo": servo_yaml}
+    servo_params = (
+        ParameterBuilder("moveit_servo")
+        .yaml(
+            parameter_namespace="moveit_servo",
+            file_path=moveit_servo_file,
+        )
+        .to_dict()
+    )
+    # servo_yaml = load_yaml(moveit_servo_file)
+    # servo_params = {"moveit_servo": servo_yaml}
     robot_description_s = {"robot_description": robot_description_arm}
     robot_description_semantic_s = {"robot_description_semantic": robot_description_semantic}
 
     servo_node = Node(
         package="moveit_servo",
-        # condition=IfCondition(launch_servo),
         executable="servo_node_main",
         parameters=[
             servo_params,
@@ -114,68 +119,22 @@ def generate_launch_description():
         output="screen",
     )
 
-    # TF information
-    robot_state_publisher_arm = Node(
-        name='robot_state_publisher',
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description_arm}],
-        remappings=[('robot_description', 'robot_description_ur5')]
-    )
-
     #  Visualization (parameters needed for MoveIt display plugin)
     rviz = Node(
         name='rviz',
         package='rviz2',
         executable='rviz2',
         output='screen',
+        arguments=["-d", moveit_rviz_file],
         parameters=[
-            {
-                'robot_description': robot_description_arm,
-                'robot_description_semantic': robot_description_semantic,
-                'robot_description_kinematics': kinematics_config,
-            }
-        ],
-    )
-    # Controller manager for realtime interactions
-    ros2_control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        name="control_node_ros2",
-        parameters= [
             {'robot_description': robot_description_arm},
-            ros_controllers_file
+            {'robot_description_semantic': robot_description_semantic},
+            {'robot_description_kinematics': kinematics_config},
         ],
-        output="screen",
     )
-
-    spawn_controllers_manipulator = Node(
-            package="controller_manager", 
-            executable="spawner",
-            name="spawner_mani",
-            arguments=['joint_trajectory_controller'],
-            output="screen")
-    
-
-    spawn_controllers_state = Node(
-            package="controller_manager", 
-            executable="spawner",
-            arguments=['joint_state_broadcaster'],
-            output="screen")
-
-
 
     return LaunchDescription([
-        robot_state_publisher_arm,
-        TimerAction(period=1.0, 
-                    actions=[spawn_controllers_manipulator,
-                            spawn_controllers_state,
-                            rviz,
-                            ros2_control_node,
-                            move_group_node,
-                            servo_node
-                            ]),
-        
-        ]
-    )
+        move_group_node,
+        servo_node,
+        rviz
+    ])
